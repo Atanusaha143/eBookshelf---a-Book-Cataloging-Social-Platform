8d83c3112a27bd9ce9b875d47a24656826385f67
snigdho611 <snigdho.howlader@gmail.com>
2021-03-06 07:52:17 +0600

    added favicon

--- home.php
+++ home.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel='icon' href='./images/favicon.png'>
+    <title>eBookShelf</title>
 </head>
 <body background="./images/background.jpg">
-    <h1>YES</h1>
+    <div>
+        <form>
+            <fieldset>
+                <legend>
+                    LOG IN
+                </legend>
+                <table>
+                    <tr>
+                        <td>Username:</td>
+                    </tr>
+                    <tr>
+                        <td><input type='text' name='username'/></td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
