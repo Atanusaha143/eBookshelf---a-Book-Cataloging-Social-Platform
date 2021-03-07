c4b7b74ed245f1c71318200e87e5a7bf0ea7355b
snigdho611 <snigdho.howlader@gmail.com>
2021-03-08 04:49:05 +0600

    added sign up form

---
+++ view/signup.php
@@ -0,0 +1,80 @@
+<?php
+    session_start();
+    if(isset($_SESSION['flag']))
+    {
+        header('location: ../controller/redirect.php');
+    }
+    else
+    {
+        
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body background="../images/assets/background.jpg">
+    <?php include('./header.php'); ?>
+    <div width='100px'>
+        <form action='../controller/logincheck.php' method="POST">
+            <fieldset>
+                <legend>
+                    <b>SIGN UP</b>
+                </legend>
+                <table align="center">
+                    <tr>
+                        <td align="right">ID:</td>
+                        <td><input type='numeric' name='id'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Full Name:</td>
+                        <td><input type='text' name='fullname'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Email:</td>
+                        <td><input type='email' name='email'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Phone:</td>
+                        <td><input type='text' name='phone'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Date of Birth:</td>
+                        <td><input type='date' name='dateOfBirth'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Username:</td>
+                        <td><input type='text' name='username'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Password:</td>
+                        <td><input type='password' name='password'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Confirm Password:</td>
+                        <td><input type='password' name='confirmpassword'/></td>
+                    </tr>
+                    <!-- <tr>
+                        <td align="right">Type of User:</td>
+                        <td><input type='password' name='confirmpassword'/></td>
+                    </tr> -->
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='submit' value="Sign up"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='reset' value="Reset"></td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
