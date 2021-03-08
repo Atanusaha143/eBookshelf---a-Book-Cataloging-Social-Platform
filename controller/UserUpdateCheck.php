039ff23eedf2aac202b86cd52d1747016d9c615e
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-08 13:34:59 +0600

    updated validation for user update info

--- controller/UserUpdateCheck.php
+++ controller/UserUpdateCheck.php
@@ -1,32 +1,23 @@
-<?php 
+<?php
+	session_start(); 
 	if(isset($_POST['update']))
 	{
-		$userFile = fopen("../model/AllUserDetails.json", "r");
-		$userData = fread($userFile, filesize('../model/AllUserDetails.json'));
-		$userInfo = json_decode($userData, true);
-
-		if($_POST['currPass'] == $userInfo['pass'])
+		if($_POST['currPass'] == $_SESSION['Password'])
 		{
 				if($_POST['newPass'] != "")
 				{
-					$file_info = $_FILES['profilePic'];
-				 	$file_path = '../resources/'.$file_info['name'];
-				 	move_uploaded_file($file_info['tmp_name'], $file_path);
+						$file_info = $_FILES['profilePic'];
+					 	$file_path = '../resources/'.$file_info['name'];
+					 	move_uploaded_file($file_info['tmp_name'], $file_path);
 
-					$userInfo = [
-								 'name' => $_POST['name'],
-								 'user' => $_POST['user'],
-								 'pass' => $_POST['newPass'],
-								 'email' => $_POST['email'],
-								 'gender' => $_POST['gender'],
-								 'phoneNumber' => $_POST['phoneNumber'],
-								 'profilePic' => $file_info['name'],
-						  ];
-					$allData = json_encode($userInfo);
-					$userData = fopen("../model/AllUserDetails.json", "w");
-					fwrite($userData, $allData);
-					fclose($userData);
-					header('location: ../view/UserEdit.php');
+						$_SESSION['Name'] = $_POST['name'];
+						$_SESSION['UserName'] = $_POST['user'];
+						$_SESSION['Password'] = $_POST['newPass'];
+						$_SESSION['Email'] = $_POST['email'];
+						$_SESSION['Gender'] = $_POST['gender'];
+						$_SESSION['PhoneNumber'] = $_POST['phoneNumber'];
+						$_SESSION['profilePic'] = $file_info['name'];
+						header('location: ../view/UserEdit.php');
 				}
 				else
 				{
@@ -34,19 +25,27 @@
 				 	$file_path = '../resources/'.$file_info['name'];
 				 	move_uploaded_file($file_info['tmp_name'], $file_path);
 
-					$userInfo = [
-								 'name' => $_POST['name'],
-								 'user' => $_POST['user'],
-								 'pass' => $userInfo['pass'],
-								 'email' => $_POST['email'],
-								 'gender' => $_POST['gender'],
-								 'phoneNumber' => $_POST['phoneNumber'],
-								 'profilePic' => $file_info['name'],
-						  ];
-					$allData = json_encode($userInfo);
-					$userData = fopen("../model/AllUserDetails.json", "w");
-					fwrite($userData, $allData);
-					fclose($userData);
+					// $userInfo = [
+					// 			 'name' => $_POST['name'],
+					// 			 'user' => $_POST['user'],
+					// 			 'pass' => $userInfo['pass'],
+					// 			 'email' => $_POST['email'],
+					// 			 'gender' => $_POST['gender'],
+					// 			 'phoneNumber' => $_POST['phoneNumber'],
+					// 			 'profilePic' => $file_info['name'],
+					// 	  ];
+					// $allData = json_encode($userInfo);
+					// $userData = fopen("../model/AllUserDetails.json", "w");
+					// fwrite($userData, $allData);
+					// fclose($userData);
+
+						$_SESSION['Name'] = $_POST['name'];
+						$_SESSION['UserName'] = $_POST['user'];
+						// $_SESSION['Password'] = $_POST['newPass'];
+						$_SESSION['Email'] = $_POST['email'];
+						$_SESSION['Gender'] = $_POST['gender'];
+						$_SESSION['PhoneNumber'] = $_POST['phoneNumber'];
+						$_SESSION['profilePic'] = $file_info['name'];
 					header('location: ../view/UserEdit.php');
 				}
 		}
