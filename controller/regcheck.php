e44fa7d34a676b38f8d2cefce0538b1ddc99e742
snigdho611 <snigdho.howlader@gmail.com>
2021-03-09 07:16:50 +0600

    Fixed all validation issues using functions

--- controller/regcheck.php
+++ controller/regcheck.php
@@ -51,6 +51,61 @@
             return false;
         }
     }
+
+    function passwordValidation($password)
+    {
+        $count = 0;
+        if(strlen($password) < 8)
+        {
+            echo "password must be atleast 8 characters long!<br>";
+            return true;
+        }
+        else if(strlen($password) >= 8)
+        {
+            for($i = 0 ; $i<strlen($password) ; $i=$i+1)
+            {
+                if(($password[$i] == '@' || $password[$i] == '#' || $password[$i] == '!' || $password[$i] == '$'))
+                {
+                    $count = $count+1;
+                }
+            }
+
+            if($count > 0)
+            {
+                return false;
+            }
+            else
+            {
+                echo "password must contain '@', '#', '!' or '$'!<br>";
+                return true;
+            }
+            // if($passwordFlag == false)
+            // {
+            //     echo "password must contain '@', '#', '!' or '$'!<br>";
+            // }
+        }
+    }
+
+    function usernameValidation($username)
+    {
+        $count = 0;
+        for($i = 0 ; $i<strlen($username) ; $i=$i+1)
+        {
+            if(!((ord($username[$i]) >= 97 && ord($username[$i]) <= 122)) 
+            && !((ord($username[$i]) >= 65 && ord($username[$i]) <= 90)) && !(ord($username[$i]) >= 48 && ord($username[$i]) <= 57))
+            {
+                $count = $count + 1;
+            }
+        }
+        if($count > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     
     if(empty($_POST['fullname']) && empty($_POST['email']) && empty($_POST['username']) && empty($_POST['password'])&& empty($_POST['dateOfBirth']) && empty($_POST['confirmpassword']))
     {
@@ -70,15 +125,8 @@
         $fullnameFlag = nameValidation($fullname);
         $emailFlag = emailValidation($email);
         $phoneFlag = phoneValidation($phone);
-        // for($i = 0 ; $i<strlen($fullname) ; $i=$i+1)
-        // {
-        //     if(!((ord($fullname[$i]) >= 97 && ord($fullname[$i]) <= 122)) 
-        //     && !((ord($fullname[$i]) >= 65 && ord($fullname[$i]) <= 90))&& !($fullname[$i] == ' '))
-        //     {
-        //         $fullnameFlag = true;
-        //         break;
-        //     }
-        // }
+        $passwordFlag = passwordValidation($password);
+        $usernameFlag = usernameValidation($username);
 
         if($fullnameFlag == true)
         {
@@ -95,41 +143,11 @@
             echo "Phone number can only have a '+' or numeric values!<br>";
         }
 
-        for($i = 0 ; $i<strlen($username) ; $i=$i+1)
-        {
-            if(!((ord($username[$i]) >= 97 && ord($username[$i]) <= 122)) 
-            && !((ord($username[$i]) >= 65 && ord($username[$i]) <= 90))&&!(ord($username[$i]) >= 48 && ord($username[$i]) <= 57))
-            {
-                $usernameFlag = true;
-                break;
-            }
-        }
-
         if($usernameFlag == true)
         {
             echo "Username must be alphanumeric!<br>";
         }
 
-        if(strlen($password) < 8)
-        {
-            echo "password must be atleast 8 characters long!<br>";
-        }
-        else if(strlen($password) >= 8)
-        {
-            for($i = 0 ; $i<strlen($password) ; $i=$i+1)
-            {
-                if(($password[$i] == '@' || $password[$i] == '#' || $password[$i] == '!' || $password[$i] == '$'))
-                {
-                    $passwordFlag = true;
-                    break;
-                }
-            }
-
-            if($passwordFlag == false)
-            {
-                echo "password must contain '@', '#', '!' or '$'!<br>";
-            }
-        }
         if($password != $confirmpassword)
         {
             $passwordFlag=false;
