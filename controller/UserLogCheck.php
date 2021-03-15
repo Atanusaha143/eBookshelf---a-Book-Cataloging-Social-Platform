939e97b49dfafcc6282e3f69cb7f3be9e0469f2a
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-15 23:28:43 +0600

    minor fixes

--- controller/UserLogCheck.php
+++ controller/UserLogCheck.php
@@ -2,45 +2,41 @@
 	session_start();
 	if(isset($_POST['login']) && filesize('../model/AllUserDetails.json')>0)
 	{
-
-		$userFile = fopen("../model/AllUserDetails.json", "r");
-		$userData = fread($userFile, filesize('../model/AllUserDetails.json'));
-		$userInfo_filter = explode("\n", $userData);
-
-		for($i=0; $i<count($userInfo_filter); $i++) 
+		if($_POST['UserName'] == "" || $_POST['Password'] == "")
 		{
+			echo "Please enter Username and Passwordd";
+		}
+		else
+		{
+			$userFile = fopen("../model/AllUserDetails.json", "r");
+			$userData = fread($userFile, filesize('../model/AllUserDetails.json'));
+			$userInfo_filter = explode("\n", $userData);
+			$flag = false;
+			for($i=0; $i<count($userInfo_filter); $i++) 
+			{
 				$userInfo = json_decode($userInfo_filter[$i], true);
-				$username = $userInfo['user'];
-				$password = $userInfo['pass'];
-
-				if($username == "" || $password == "")
+				if(isset($userInfo['user']) && isset($userInfo['pass']) && $userInfo['user'] == $_POST['UserName'] && $userInfo['pass'] == $_POST['Password'])
 				{
-					echo "Please enter Username and Passwordd";
+					$flag = true;
+					$_SESSION['flag'] = true;
+					$_SESSION['Name'] = $userInfo['name'];
+					$_SESSION['UserName'] = $userInfo['user'];
+					$_SESSION['Password'] = $userInfo['pass'];
+					$_SESSION['Email'] = $userInfo['email'];
+					$_SESSION['Gender'] = $userInfo['gender'];
+					$_SESSION['PhoneNumber'] = $userInfo['phoneNumber'];
+					if(isset($_POST['rememberMe']))
+					{
+						setcookie('checkLogin', true, time()+86400, "/");
+						setcookie('username', $userInfo['user'], time()+86400, "/");
+					}
+					header('location: ../view/UserHome.php');
 				}
-				else
-				{
-
-					if($username == $_POST['UserName'] && $password == $_POST['Password'])
-					{
-						$_SESSION['flag'] = true;
-						$_SESSION['Name'] = $userInfo['name'];
-						$_SESSION['UserName'] = $userInfo['user'];
-						$_SESSION['Password'] = $userInfo['pass'];
-						$_SESSION['Email'] = $userInfo['email'];
-						$_SESSION['Gender'] = $userInfo['gender'];
-						$_SESSION['PhoneNumber'] = $userInfo['phoneNumber'];
-						if(isset($_POST['rememberMe']))
-						{
-							setcookie('checkLogin', true, time()+86400, "/");
-							setcookie('username', $userInfo['user'], time()+86400, "/");
-						}
-						header('location: ../view/UserHome.php');
-					}
-					else
-					{
-						echo "Invalid Username and Password!";
-					}
-				} 
+			}
+			if($flag == false)
+			{
+				echo "Invalid Username and Password!";
+			}
 		}
 	}
 	else
