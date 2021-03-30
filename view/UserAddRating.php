cc3a0f3a83bfb14447e76ca0c323120744ff16d2
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-30 17:51:26 +0600

    added some css

--- view/UserAddRating.php
+++ view/UserAddRating.php
@@ -40,9 +40,9 @@
 		<tr>
 			<td align="right" colspan="3">
 				<a href="UserHome.php"> <img src="../resources/logo.png" align="left"  width="100%" height="150"> </a>
-				<a href="UserHome.php"> Go Back </a>
+				<a href="UserHome.php" class="linkBtn"> Go Back </a>
 				&nbsp | &nbsp
-				<a href="UserLogout.php"> Logout </a>
+				<a href="UserLogout.php" class="linkBtn"> Logout </a>
 				&nbsp
 			</td>
 		</tr>
@@ -54,7 +54,7 @@
 						<b>Book Name:</b> The 7 Habits of Highly Effective People <br><br>
 						<b>Author:</b> Stephen Covey <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating1" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating1" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -62,7 +62,7 @@
 						<b>Book Name:</b> How to Win Friends & Influence People <br><br>
 						<b>Author:</b> Dale Carnegie <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating2" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating2" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -70,7 +70,7 @@
 						<b>Book Name:</b> Think and Grow Rich <br><br>
 						<b>Author:</b> Napoleon Hill <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating3" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating3" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -78,7 +78,7 @@
 						<b>Book Name:</b> Awaken the Giant Within <br><br>
 						<b>Author:</b> Tony Robbins <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating4" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating4" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -86,7 +86,7 @@
 						<b>Book Name:</b> As a Man Thinketh <br><br>
 						<b>Author:</b> James Allen <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating5" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating5" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -94,7 +94,7 @@
 						<b>Book Name:</b> The Greatest Salesman in the World <br><br>
 						<b>Author:</b> Og Mandino <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating6" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating6" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -102,7 +102,7 @@
 						<b>Book Name:</b> Don't Sweat the Small Stuff <br><br>
 						<b>Author:</b> Richard Carlson <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating7" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating7" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -110,7 +110,7 @@
 						<b>Book Name:</b> Driv <br><br>
 						<b>Author:</b> Daniel H. Pink <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating8" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating8" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -118,7 +118,7 @@
 						<b>Book Name:</b> The Power of Positive Thinking <br><br>
 						<b>Author:</b> Norman Vincent Peale <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating9" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating9" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 				<fieldset>
@@ -126,7 +126,7 @@
 						<b>Book Name:</b> The Magic of Thinking Big <br><br>
 						<b>Author:</b> David J. Schwartz <br> <br>
 						<b>Add Rating:</b> <input type="radio" name="raring1"> <input type="radio" name="raring2"> <input type="radio" name="raring3"> <input type="radio" name="raring4"> <input type="radio" name="raring5"> <br> <br> 
-						<input type="submit" name="rating10" value="Add" style="margin-left: 6em">
+						<input type="submit" name="rating10" value="Add" style="margin-left: 6em" class="submitBtn">
 					</form>
 				</fieldset>
 			</td>
