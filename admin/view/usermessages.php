d286d7093aadb19c152c66c6665ace78aa2895fe
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-08 05:45:09 +0600

    Added a page for messages with user

---
+++ admin/view/usermessages.php
@@ -0,0 +1,40 @@
+<?php  
+    $dataString = file_get_contents('../../model/messages.json');
+    $dataJSON = json_decode($dataString, true);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='icon' href="../../assets/images/icon.png">
+    <link rel='stylesheet' href="../../assets/resources/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <table border="1px solid black" width='80%'>
+            <?php 
+                foreach($dataJSON as $message)
+                {
+                    echo
+                    "<tr>
+                        <td>
+                            ".$message['from']."
+                            <br>
+                            ".$message['time']."
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'>
+                            ".$message['content']."
+                        </td>
+                    </tr>";
+                }
+            ?>
+        </table>
+    </center>
+</body>
+</html>
