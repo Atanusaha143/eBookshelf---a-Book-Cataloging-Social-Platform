526665fdc7d803a4dcb290856cf25fe323569e72
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-08 10:24:11 +0600

    Added message feature

--- admin/view/messages.php
+++ admin/view/messages.php
@@ -1,8 +1,28 @@
-<?php  
+<?php 
     session_start();
-    include('../model/dbCon.php');
+    include('../model/adminModel.php');
+    if(!empty($_SESSION['flag']) && isset($_COOKIE['flag']))
+    {
+        //continue
+    }
+    else if(!(isset($_COOKIE['flag'])))
+    {
+        echo "Session expired, please <a href='./login.php'>Log In</a> again!";
+        return;
+    }
+    else
+    {
+        header('location: ./login.php');
+    }
+?>
+
+<?php  
+    //include('../model/dbCon.php');
     
     $connection = connect();
+    $sql = "select * from messages where id in (select max(id) from messages where to_user = ".$_SESSION['id']." GROUP by from_user)";
+    $result = mysqli_query($connection, $sql);
+    //$result = mysqli_fetch_assoc($result);
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +38,32 @@
 <body>
     <?php include('./header.php');?>
     <?php include('./navbar.php');?>
-    
+    <div class='messageBar'>
+        <table border="1">
+            <?php 
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $sqlfullname = "select DISTINCT admin.fullname from admin, messages where admin.id = ".$row['from_user'];
+                $fullname = mysqli_query($connection, $sqlfullname);
+                $fullname = mysqli_fetch_assoc($fullname);
+
+                echo "<tr>
+                <td>
+                    ".$row['time']."
+                </td>
+                <td rowspan='2'>"
+                    .$row['content'].
+                    "
+                </td>
+                </tr>
+                <tr>
+                    <td>
+                        ".$fullname['fullname']."
+                    </td>
+                </tr>
+                <br>"; 
+            }?>   
+        </table>
+    </div>
 </body>
 </html>
