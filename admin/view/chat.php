cf084235d442343801b6aa39040a2ed6ecf7118a
snigdho611 <snigdho.howlader@gmail.com>
2021-04-10 07:28:39 +0600

    Added chat.php

--- admin/view/chat.php
+++ admin/view/chat.php
@@ -1,9 +1,8 @@
-<?php
+<?php 
     session_start();
-    if(!empty($_SESSION['flag']) && isset($_COOKIE['flag']))
+    if(isset($_SESSION['flag']) && isset($_COOKIE['flag']))
     {
         //continue
-        //include('../../controller/admin/validateviewprofile.php');
     }
     else if(!(isset($_COOKIE['flag'])))
     {
@@ -16,7 +15,6 @@
         header('location: ./login.php');
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,18 +23,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='icon' href="../../assets/images/icon.png">
     <link rel='stylesheet' href="../../assets/resources/style.css">
-    <title>Send a message</title>
+    <title>Add User</title>
 </head>
-<body bgcolor="#c5fcf7">
+<body>
     <?php include('./header.php'); ?>
     <?php include('./navbar.php'); ?>
-    <center>
-        <form method='POST' action='./chat.php' class="form">
-            <h3>Send a message to <input type='text' class='messageUsername'></h3>
-            <textarea placeholder="Write your message here" name='message'></textarea><br>
-            <input type="submit" value='Send'>
-        </form>
-    </center>
-    <?php include('./footer.php') ?>
+    <div width='100px' class='form'>
+        
+    </div>
+    <?php include('./footer.php'); ?>
 </body>
 </html>
