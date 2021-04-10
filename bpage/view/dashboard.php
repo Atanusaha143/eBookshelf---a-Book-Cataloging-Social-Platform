e26272c49ea2e784df50e51fe3e22c5d585b7729
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-10 17:05:56 +0600

    Updates to dashboard

--- bpage/view/dashboard.php
+++ bpage/view/dashboard.php
@@ -23,7 +23,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='icon' href='../images/assets/icon.png'>
+    <link rel='stylesheet' href='../../assets/images/icon.png'>
+    <link rel='stylesheet' href='../../assets/resources/style.css'>
     <title>Dashboard</title>
 </head>
 <body bgcolor="#c5fcf7">
