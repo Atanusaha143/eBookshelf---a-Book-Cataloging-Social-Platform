bd5b054740bb3327fd1fa55f40ff85fb7427f1df
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-10 16:54:19 +0600

    Updated page design

--- bpage/view/login.php
+++ bpage/view/login.php
@@ -13,12 +13,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='icon' href='../images/assets/icon.png'>
+    <link rel='stylesheet' href='../../assets/resources/style.css'>
     <title>eBookShelf</title>
 </head>
 <body bgcolor="#c5fcf7">
     <?php include('./header.php'); ?>
     <center>
-        <div>
+        <div class='form'>
             <form action='../controller/logincheck.php' method="POST">
                 <fieldset>
                     <legend>
@@ -55,6 +56,6 @@
         </div>
     </center>
     
-    <?php include('./footer.php'); ?>
+    <?php include('../../assets/footer.php'); ?>
 </body>
 </html>
