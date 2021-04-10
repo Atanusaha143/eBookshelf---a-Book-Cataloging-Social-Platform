6e960533130f899e5bbf40c387a79bd2d4f073ea
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-11 05:00:18 +0600

    Updated so that user cannot send messages to non-existent user

--- admin/controller/checkchat.php
+++ admin/controller/checkchat.php
@@ -1,8 +1,19 @@
 <?php
     session_start();
+    include('../model/adminModel.php');
     if(isset($_POST['username']))
     {   
-        $link = 'location: ../view/chat.php?username='.$_POST['username'];
-        header($link);
+        $adminDetails = getAdminInfoByUsername($_POST['username']);
+        $adminDetails = mysqli_fetch_assoc($adminDetails);
+        if($adminDetails == false)
+        {
+            echo "It seems the user you are looking for does not exist!";
+        }
+        else
+        {
+            //$link = 'location: ../view/chat.php?username='.$_POST['username'];
+            //header($link);
+            print_r($adminDetails);
+        }
     }
 ?>
