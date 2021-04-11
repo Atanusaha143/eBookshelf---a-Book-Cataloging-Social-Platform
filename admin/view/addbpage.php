9206240b7fda0d3e283000f5d9db3f90482144d6
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-11 12:42:29 +0600

    Updated bpage user registration page

---
+++ admin/view/addbpage.php
@@ -0,0 +1,77 @@
+<?php
+    session_start();
+    if(isset($_SESSION['flag']) && isset($_COOKIE['flag']))
+    {
+        //continue
+    }
+    else if(!(isset($_COOKIE['flag'])))
+    {
+        header('location: ./expired.php');
+        // echo "Session expired, please <a href='./login.php'>Log In</a> again!";
+        // return;
+    }
+    else
+    {
+        header('location: ./login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='icon' href='../../assets/images/icon.png'>
+    <link rel='stylesheet' href='../../assets/resources/style.css'>
+    <title>Sign Up</title>
+</head>
+<body bgcolor="#c5fcf7">
+    <?php include('./header.php'); ?>
+    <div class='form'>
+        <form action='../controller/regcheck.php' method="POST">
+                <b>SIGN UP</b>
+                <table align="center">
+                    <tr>
+                        <td align="right">Business Name:</td>
+                        <td><input type='text' name='fullname'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Email:</td>
+                        <td><input type='email' name='email'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Phone:</td>
+                        <td><input type='text' name='phone' value="+88"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Username:</td>
+                        <td><input type='text' name='username'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Password:</td>
+                        <td><input type='text' name='password'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Confirm Password:</td>
+                        <td><input type='text' name='confirmpassword'/></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='submit' value="Sign up"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='reset' value="Reset"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2">
+                            <a href='./login.php'>Go Back</a>
+                        </td>
+                    </tr>
+                </table>
+        </form>
+    </div>
+</body>
+</html>
