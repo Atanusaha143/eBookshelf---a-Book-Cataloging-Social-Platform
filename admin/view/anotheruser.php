c6fd6c2e592230ff108fc5d9b707d97a10d305a7
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-11 12:37:16 +0600

    Added option viewing for other users

--- admin/view/anotheruser.php
+++ admin/view/anotheruser.php
@@ -5,7 +5,8 @@
     {
         //continue
         //include('../controller/validateviewprofile.php');
-        $adminDetails = getAdminInfoByID($_GET['id']);
+        $adminDetails = getAdminInfoByID($_GET['userid']);
+        $adminDetails = mysqli_fetch_assoc($adminDetails);
     }
     else if(!(isset($_COOKIE['flag'])))
     {
@@ -27,81 +28,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='icon' href="../../assets/images/icon.png">
     <link rel='stylesheet' href="../../assets/resources/style.css">
-    <title><?php echo $_GET['fullname']; ?></title>
+    <title><?php echo $adminDetails['fullname']; ?></title>
 </head>
 <body bgcolor="#c5fcf7">
     <?php include('./header.php'); ?>
     <?php include('./navbar.php'); ?>
-    <table border="1px solid black" width='100%'>
-        <tr>
-            <td>
-                <table align="center" border="1px solid black">
-                    <tr>
-                        <td align="right">
-                            <b>User Type:</b>
-                        </td>
-                        <td width='40%'>
-                            <?php echo $_GET['type']; ?>
-                        </td>
-                        <td rowspan="6">
-                            <img src= '<?php echo "../images/profile/admin/".$_GET['id'].".jpeg"; ?>' height="250">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <b>Name:</b>
-                        </td>
-                        <td>
-                            <?php echo $_GET['fullname']; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <b>Email:</b>
-                        </td>
-                        <td>
-                            <?php echo $_GET['email']; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <b>Username:</b>
-                        </td>
-                        <td>
-                            <?php echo $_GET['username']; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <b>Date of Birth:</b>
-                        </td>
-                        <td>
-                            <?php echo $_GET['dateOfBirth']; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <b>Registration Date:</b>
-                        </td>
-                        <td>
-                            <?php echo $_GET['regdate']; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="3" align="center">
-                            <a href="./sendmessage.php?userid=<?php echo $_GET['userid']; ?>">Send a message</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="3" align="center">
-                            <a href="./allusers.php">Go Back</a>
-                        </td>
-                    </tr>
-                </table>
-                <br>
-            </td>
-        </tr>
-    </table>
+    <div class='data'>
+        <table width='100%'>
+            <tr>
+                <td>
+                    <table align="center" border="1px solid black">
+                        <tr>
+                            <td align="right">
+                                <b>User Type:</b>
+                            </td>
+                            <td width='40%'>
+                                <?php echo $_SESSION['type']; ?>
+                            </td>
+                            <td rowspan="6">
+                                <img src= '<?php echo "../../assets/profile/admin/".$adminDetails['photo']; ?>' height="250">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <b>Name:</b>
+                            </td>
+                            <td>
+                                <?php echo $adminDetails['fullname']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <b>Email:</b>
+                            </td>
+                            <td>
+                                <?php echo $adminDetails['email']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <b>Username:</b>
+                            </td>
+                            <td>
+                                <?php echo $adminDetails['username']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <b>Date of Birth:</b>
+                            </td>
+                            <td>
+                                <?php echo $adminDetails['dob']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <b>Registration Date:</b>
+                            </td>
+                            <td>
+                                <?php echo $adminDetails['regdate']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" align="center">
+                                <a href="./sendmessage.php?userid=<?php echo $adminDetails['id']; ?>">Send a message</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" align="center">
+                                <a href="./allusers.php">Go Back</a>
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                </td>
+            </tr>
+        </table>
+    </div>
     <?php include('./footer.php'); ?>
 </body>
 </html>
