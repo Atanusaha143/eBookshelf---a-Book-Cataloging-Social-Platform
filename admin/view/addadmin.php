a374ddffdbfbd9a86bb1536780ca9b83e5a413b9
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-11 11:30:44 +0600

    Turned password fields into password type

--- admin/view/addadmin.php
+++ admin/view/addadmin.php
@@ -53,11 +53,11 @@
                 </tr>
                 <tr>
                     <td align="right"><label>Password:</label></td>
-                    <td><input type='text' name='password'/></td>
+                    <td><input type='password' name='password'/></td>
                 </tr>
                 <tr>
                     <td align="right"><label>Confirm Password:</label></td>
-                    <td><input type='text' name='confirmpassword'/></td>
+                    <td><input type='password' name='confirmpassword'/></td>
                 </tr>
                 <tr>
                     <td>
