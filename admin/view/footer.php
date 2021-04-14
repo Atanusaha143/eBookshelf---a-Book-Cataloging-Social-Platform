91d37cd1a3d601e103969c6e0de5572ab72653f5
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-15 03:25:54 +0600

    Updated contact.php

--- admin/view/footer.php
+++ admin/view/footer.php
@@ -1,7 +1,7 @@
 <div class='footer'>
     <label>
         <b>
-            Authorized under copyright © 2021
+            Authorized under copyright © 2021 <a href="./contact.php">Contact Us</a>
         </b>
     </label>   
 </div>
