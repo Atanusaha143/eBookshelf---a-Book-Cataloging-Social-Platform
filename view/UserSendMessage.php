611425e2e2026578bf6450b68739819be19ee2f8
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:40:31 +0600

    updated user send message with DB

--- view/UserSendMessage.php
+++ view/UserSendMessage.php
@@ -11,39 +11,19 @@
 	include ('header.php');
 ?>
 
-<?php
-	if(isset($_POST['send']))
-	{
-		if($_POST['message'] != "")
-		{
-			echo "Your message has been sent to the desired person!";
-		}
-		else
-		{
-			echo "Please write a message";
-		}
-	}
-	if(isset($_POST['search']))
-	{
-		if($_POST['searchBox'] == "")
-		{
-			echo "Please write someone name into the search box!";
-		}
-		else
-		{
-			echo "Searched";
-		}
-	}
-?>
 	<table border="1" width="100%" cellspacing="0">
 		<tr>
 			<td align="right" colspan="3">
-				<a href="UserHome.php"> <img src="../resources/logo.png" align="left" width="100%" height="150"> </a>
+				<a href="UserHome.php"> <img src="../resources/banner.png" align="left" width="100%" height="150"> </a>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
 				<br>
+				<a href="UserReceiveMessage.php" class="linkBtn updateBtn" style="background-color: #999900"> View Received Messages </a>
+				&nbsp | &nbsp
+				<a href="UserSentMessage.php" class="linkBtn updateBtn"> View Sent Messages </a>
+				&nbsp | &nbsp
 				<a href="UserHome.php" class="linkBtn gobackBtn"> Go Back </a>
 				&nbsp | &nbsp
 				<a href="UserLogout.php" class="linkBtn logoutBtn"> Logout </a>
@@ -54,23 +34,20 @@
 		<tr height = "200px">
 			<td colspan="2" align="center">
 				<br>
-				<form method="POST" action="">
+				<form method="POST" action="../controller/UserSendMessageCheck.php">
 					<fieldset style="width: 50%" class="fieldSetBorder">
 					<legend> <b> Send Message </b> </legend>
 					<table border="0" width="60%">
 						<tr>
 							<td>
-								<label for="search" style="margin-left: 4em"></label>
-								<input id="search" type="search" style="width: 55%" name="searchBox" placeholder=" search someone...">
-								<input type="submit" name="search" value="Search" class="submitBtn" style="padding: 5px 20px">
+								<input type="search" style="width: 55%; margin-left:7em" name="searchBox" placeholder="Type a name...">
 							</td>
 						</tr>
 						<tr>
 							<td align="center">
 								<br>
-								<label for="message" style="margin-left: 1em"> Write Message </label>
-									<input  id="message" type="text" name="message" style="height: 100px;">
-								&nbsp <input id="message" type="submit" name="send" value="Send" class="submitBtn" style="padding: 5px 20px">
+									Message: <input type="text" name="message" style="height: 100px;">
+								&nbsp <input type="submit" name="send" value="Send" class="submitBtn" style="padding: 5px 20px">
 							</td>
 						</tr>
 					</table>
@@ -79,8 +56,8 @@
 				<br> 
 			</td>
 		</tr>
-		<tr height = "50px">
-			<td colspan="3">
+		<tr height = "50px" style="background-color: #333; color: white;">
+			<td colspan="3" style="padding: 25px;">
 				<center> eBookshelf &copy 2021 </center>
 			</td>
 		</tr>
