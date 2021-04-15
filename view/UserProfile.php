d4e11469b314522680b0fa4a984cfbbdefa8c79f
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:40:11 +0600

    updated user profile with DB

--- view/UserProfile.php
+++ view/UserProfile.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+	/*session_start();
 	if(!isset($_SESSION['flag']))
 	{
 		header('location: ../');
@@ -105,8 +105,131 @@
 				<br> 
 			</td>
 		</tr>
-		<tr height = "50px">
-			<td colspan="3">
+		<tr height = "50px" style="background-color: #333; color: white;">
+			<td colspan="3" style="padding: 25px;">
+				<center> eBookshelf &copy 2021 </center>
+			</td>
+		</tr>
+	</table>
+<?php
+	include ('footer.php');*/
+?>
+
+
+
+
+<!-- Using DB -->
+<?php
+	session_start();
+	if(!isset($_SESSION['flag']))
+	{
+		header('location: ../');
+	}
+?>
+
+<?php
+	$title = "Profile";
+	include ('header.php');
+?>
+	<table border="1" width="100%" cellspacing="0">
+		<tr>
+			<td align="right" colspan="3">
+				<a href="UserHome.php"> <img src="../resources/banner.png" align="left" width="100%" height="150"> </a>
+			</td>
+		</tr>
+		<tr>
+			<td align="right">
+				<br>
+				<a href="UserHome.php" class="linkBtn gobackBtn"> Go Back </a>
+				&nbsp | &nbsp
+				<a href="UserLogout.php" class="linkBtn logoutBtn"> Logout </a>
+				<br>
+				&nbsp
+			</td>
+		</tr>
+		<?php
+				require_once('../model/dbConnection.php');
+				require_once('../model/userModel.php');
+				$username = $_SESSION['UserName'];
+				$userInfo = getUserByUsername($username);
+		?>
+		<tr height = "200px">
+			<td colspan="2" align="center">
+				<br>
+				<fieldset style="width: 50%" class="fieldSetBorder">
+					<legend> <b> PROFILE </b> </legend>
+					<table border="0">
+						<tr>
+							<td>
+								<b>Name:</b>
+							</td>
+							<td colspan="2">
+								<?php echo $userInfo['name']; ?>
+							</td>
+							<td rowspan="5">
+								&nbsp &nbsp &nbsp &nbsp &nbsp
+							</td>
+							<td>
+								<?php 
+										$path = '../resources/img/User/'.$userInfo['profile_photo']; 
+										echo '&nbsp &nbsp &nbsp &nbsp <img src="'.$path .'" alt="No Profile Picture" height="200px" />';
+								?>
+							</td>
+						</tr>
+						<tr> <td colspan="2"> <hr> </td> </tr>
+
+						<tr>
+							<td>
+								<b>Username:</b>
+							</td>
+							<td colspan="2">
+								<?php echo $userInfo['username']; ?>
+							</td>
+						</tr>
+						<tr> <td colspan="2"> <hr> </td> </tr>
+
+						<tr>
+							<td>
+								<b>Email:</b>
+							</td>
+							<td>
+								<?php echo $userInfo['email']; ?>
+							</td>
+						</tr>
+						<tr> <td colspan="2"> <hr> </td> </tr>
+
+						<tr>
+							<td>
+								<b>Phone Number:</b>
+							</td>
+							<td>
+								<?php echo $userInfo['phone_number']; ?>
+							</td>
+						</tr>
+						<tr> <td colspan="2"> <hr> </td> </tr>
+
+						<tr>
+							<td>
+								<b>Gender:</b>
+							</td>
+							<td>
+								<?php echo $userInfo['gender']; ?>
+							</td>
+						</tr>
+
+						<tr> <td colspan="4"> <hr> </td> </tr>
+						<tr>
+							<td colspan="2">  
+								<center> <a href="UserEdit.php" class="linkBtn submitBtn" style="padding: 5px 20px"> Edit Profile </a> </center>
+							</td>
+						</tr>
+					</table>
+				</fieldset>
+				<br> 
+			</td>
+		</tr>
+		<tr height = "50px" style="background-color: #333; color: white;">
+			<td colspan="3" style="padding: 25px;">
 				<center> eBookshelf &copy 2021 </center>
 			</td>
 		</tr>
