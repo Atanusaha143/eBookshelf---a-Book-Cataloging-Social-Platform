8ccf80aebec13dedc44d02b73260c347e5e119ce
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:38:53 +0600

    updated user edit design view with DB

--- view/UserEdit.php
+++ view/UserEdit.php
@@ -13,19 +13,25 @@
 	<table border="1" width="100%" cellspacing="0">
 		<tr>
 			<td align="right" colspan="3">
-				<a href="UserHome.php"> <img src="../resources/logo.png" align="left" width="100%" height="150"> </a>
+				<a href="UserHome.php"> <img src="../resources/banner.png" align="left" width="100%" height="150"> </a>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
 				<br>
-				<a href="UserHome.php" class="linkBtn gobackBtn"> Go Back </a>
+				<a href="UserProfile.php" class="linkBtn gobackBtn"> Go Back </a>
 				&nbsp | &nbsp
 				<a href="UserLogout.php" class="linkBtn logoutBtn"> Logout </a>
 				<br>
 				&nbsp
 			</td>
 		</tr>
+		<?php
+			require_once('../model/dbConnection.php');
+			require_once('../model/userModel.php');
+			$username = $_SESSION['UserName'];
+			$userInfo = getUserByUsername($username);
+		?>
 		<tr height = "200px">
 			<td colspan="2" align="center">
 				<br>
@@ -38,22 +44,18 @@
 									<b>Name:</b>
 								</td>
 								<td colspan="2">
-									<input type="text" name="name" value="<?php echo $_SESSION['Name']; ?>">
+									<input type="text" name="name" value="<?php echo $userInfo['name']; ?>">
 								</td>
 								<td rowspan="5">
 
 									&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <b> Upload Photo</b>
 									<br> <br>
 									<?php 
-										if(isset($_SESSION['profilePic']))
-										{
-											$path = '../resources/'.$_SESSION['profilePic']; 
-											echo '&nbsp &nbsp &nbsp &nbsp <img src="'.$path .'" alt="No Profile Picture" height="200px" />';
-										}
-										else echo "	&nbsp &nbsp &nbsp &nbsp No Profile Picture";
+										$path = '../resources/img/User/'.$userInfo['profile_photo']; 
+										echo '&nbsp &nbsp &nbsp &nbsp <img src="'.$path .'" alt="No Profile Picture" height="200px" />';
 									 ?>
 									<br> <br>
-									&nbsp &nbsp &nbsp &nbsp &nbsp <input type="file" name="profilePic">
+									&nbsp &nbsp &nbsp &nbsp &nbsp <input type="file" name="profilePic" value="$userInfo['profile_photo']">
 								</td>
 							</tr>
 							<tr> <td colspan="2"> <hr> </td> </tr>
@@ -63,7 +65,7 @@
 									<b>Username:</b>
 								</td>
 								<td>
-									<input type="text" name="user" size="50%" value="<?php echo $_SESSION['UserName']; ?>" >
+									<input type="text" name="user" size="50%" value="<?php echo $userInfo['username']; ?>" >
 								</td>
 							</tr>
 							<tr> <td colspan="2"> <hr> </td> </tr>
@@ -93,7 +95,7 @@
 									<b>Email:</b>
 								</td>
 								<td>
-									<input type="email" name="email" size="50%" value="<?php echo $_SESSION['Email']; ?>" >
+									<input type="email" name="email" size="50%" value="<?php echo $userInfo['email']; ?>" >
 								</td>
 							</tr>
 							<tr> <td colspan="2"> <hr> </td> </tr>
@@ -104,9 +106,9 @@
 								</td>
 								<td>
 									<select name="gender">
-										<option value="Male" <?php if($_SESSION['Gender'] == "Male") echo "selected"; ?> > Male </option>
-										<option value="Female" <?php if($_SESSION['Gender'] == "Female") echo "selected"; ?>> Female </option>
-										<option value="Other" <?php if($_SESSION['Gender'] == "Other") echo "selected"; ?> > Other </option>
+										<option value="Male" <?php if($userInfo['gender'] == "Male") echo "selected"; ?> > Male </option>
+										<option value="Female" <?php if($userInfo['gender'] == "Female") echo "selected"; ?>> Female </option>
+										<option value="Other" <?php if($userInfo['gender'] == "Other") echo "selected"; ?> > Other </option>
 									</select>
 								</td>
 							</tr>
@@ -117,7 +119,7 @@
 									<b>Phone Number:</b>
 								</td>
 								<td>
-									<input type="text" name="phoneNumber" size="50%" value="<?php echo $_SESSION['PhoneNumber']; ?>" >
+									<input type="text" name="phoneNumber" size="50%" value="<?php echo $userInfo['phone_number']; ?>" >
 								</td>
 							</tr>
 							<tr> <td colspan="2"> <hr> </td> </tr>
@@ -132,8 +134,8 @@
 				<br> 
 			</td>
 		</tr>
-		<tr height = "50px">
-			<td colspan="3">
+		<tr height = "50px" style="background-color: #333; color: white;">
+			<td colspan="3" style="padding: 25px;">
 				<center> eBookshelf &copy 2021 </center>
 			</td>
 		</tr>
