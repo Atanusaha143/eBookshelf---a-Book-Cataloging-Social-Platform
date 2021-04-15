eb0e73a50b1f59451ba12afb63e554fb71d09de8
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:38:16 +0600

    updated bookshelf clean design view with DB

--- view/UserBookshelfClean.php
+++ view/UserBookshelfClean.php
@@ -1,6 +1,17 @@
-<?php
-	session_start();
+<!-- using Json -->
+<!-- <?php
+	/*session_start();
 	$clean_file = fopen("../model/".$_SESSION['Name']."Bookshelf".".json",'w');
 	fclose($clean_file);
+	header('location: ../view/userBookList.php');*/
+?> -->
+
+
+<!-- using DB -->
+<?php
+	session_start();
+	require_once('../model/dbConnection.php');
+	require_once('../model/userModel.php');
+	$clearBookshelf = clearBookshelf($_SESSION['UserName']);
 	header('location: ../view/userBookList.php');
 ?>
