c667d529cb7e58f52e275cc37da79df4d3685025
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:33:11 +0600

    updated with server side validation

--- controller/UserPostCheck.php
+++ controller/UserPostCheck.php
@@ -1,5 +1,6 @@
-<?php
-	session_start();
+<!-- Using Json -->
+<!-- <?php
+	/*session_start();
 	if(isset($_POST['post']))
 	{
 		if($_POST['bookName'] == "") echo "Please write book name";
@@ -27,6 +28,43 @@
 			$activity_data = fopen("../model/$user_name"."AllActivity".".json", "a");
 			fwrite($activity_data, $activity_encode."\r\n");
 			fclose($activity_data);
+
+			header('location: ../view/UserHome.php');
+		}
+	}*/
+?> -->
+
+
+
+<!-- Using DB -->
+<?php
+	session_start();
+	if(isset($_POST['post']))
+	{
+		if($_POST['bookName'] == "") echo "Please write book name";
+		elseif($_POST['bookAuthor'] == "") echo "Please write author name";
+		elseif($_POST['postContent'] == "") echo "Please write post content";
+		elseif (!empty($_POST['bookName']) && !empty($_POST['bookAuthor']) && !empty($_POST['postContent']))
+		{
+			date_default_timezone_set('Asia/Dhaka');
+			$book_name = $_POST['bookName'];
+			$author = $_POST['bookAuthor'];
+			$post_content = $_POST['postContent'];
+			$post_category = $_POST['Category'];
+			$user_name = $_SESSION['UserName'];
+			$date_time = date('Y-m-d H:i:s');
+
+			$postDetails = array( "bookname" => $book_name, "authorname" => $author, "category" => $post_category, "postcontent" => $post_content, "username" => $user_name);
+
+			$activityDetails = array("type" => "Create Post", "time" => $date_time, "details" => $book_name, "username" => $user_name);
+
+			$bookDetails = array( "bookname" => $book_name, "authorname" => $author, "category" => $post_category, "details" => $post_content, "username" => $user_name);
+
+			require_once('../model/dbConnection.php');
+			require_once('../model/userModel.php');
+			$postInDB = insertPost($postDetails);
+			$activityInDB = insertActivity($activityDetails);
+			//$allBooksInDB = insertAllBooks($bookDetails);
 
 			header('location: ../view/UserHome.php');
 		}
