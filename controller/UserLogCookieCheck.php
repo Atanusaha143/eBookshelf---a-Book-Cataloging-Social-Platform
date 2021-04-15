ee5839ab7ba5a2bc0751308251fec78b8a421b17
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-15 20:32:51 +0600

    updated with server side validation

--- controller/UserLogCookieCheck.php
+++ controller/UserLogCookieCheck.php
@@ -3,24 +3,23 @@
 	if(isset($_COOKIE['checkLogin']))
 	{
 
-		$userFile = fopen("../model/AllUserDetails.json", "r");
-		$userData = fread($userFile, filesize('../model/AllUserDetails.json'));
-		$userInfo_filter = explode("\n", $userData);
+		require_once('../model/dbConnection.php');
+		require_once('../model/userModel.php');
+		$userList = getAllUser();
 
-		for($i=0; $i<count($userInfo_filter); $i++) 
+		for($i=0; $i<count($userList); $i++) 
 		{
-			$userInfo = json_decode($userInfo_filter[$i], true);
-			$username = $userInfo['user'];
+			$username = $userList[$i]['username'];
 
 			if($username == $_COOKIE['username'])
 			{
 				$_SESSION['flag'] = true;
-				$_SESSION['Name'] = $userInfo['name'];
-				$_SESSION['UserName'] = $userInfo['user'];
-				$_SESSION['Password'] = $userInfo['pass'];
-				$_SESSION['Email'] = $userInfo['email'];
-				$_SESSION['Gender'] = $userInfo['gender'];
-				$_SESSION['PhoneNumber'] = $userInfo['phoneNumber'];
+				$_SESSION['Name'] = $userList[$i]['name'];
+				$_SESSION['UserName'] = $userList[$i]['username'];
+				$_SESSION['Password'] = $userList[$i]['password'];
+				$_SESSION['Email'] = $userList[$i]['email'];
+				$_SESSION['Gender'] = $userList[$i]['gender'];
+				$_SESSION['PhoneNumber'] = $userList[$i]['phone_number'];
 				header('location: ../view/UserHome.php');
 			} 
 		}
