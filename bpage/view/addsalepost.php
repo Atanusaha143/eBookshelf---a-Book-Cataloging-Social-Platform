d51e76e11e04abff3c07ba11aaa5e32519f993ee
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-04-24 22:22:48 +0600

    Added a post sale form

--- bpage/view/addsalepost.php
+++ bpage/view/addsalepost.php
@@ -29,5 +29,46 @@
 <body>
     <?php include('./header.php'); ?>
     <?php include('./navbar.php'); ?>
+    <div class='form'>
+        <form action='../controller/regcheck.php' method="POST" enctype="multipart/form-data">
+                <h3>Let the world know about your next sale!</h3>
+                <table align="center">
+                    <tr>
+                        <td align="right"><label>Book Title:</label></td>
+                        <td><input type='text' name='name'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label>Author:</label></td>
+                        <td><input type='text' name='author'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label>Synopsis:</label></td>
+                        <td><input type='text' name='post_content' value=""/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label>Price:</label></td>
+                        <td><input type='text' name='price' value='$'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label>Photo:</label></td>
+                        <td><input type='file' name='bookphoto'/></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='submit' value="Sign up"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='reset' value="Reset"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2">
+                            <a href='./login.php'>Go Back</a>
+                        </td>
+                    </tr>
+                </table>
+        </form>
+    </div>
 </body>
 </html>
